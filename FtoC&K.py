71cd48a230a119039681b2303249d4829ba433e6
lhospidales <59913982+lhospidales@users.noreply.github.com>
2020-01-15 03:20:24 -0600

    Update FtoC&K.py
    Fahrenheit to Celsius and Kelvin

--- FtoC&K.py
+++ FtoC&K.py
@@ -6,12 +6,12 @@ Created on Wed Jan 15 03:00:20 2020
 @author: liam
 """
 
-#Don't have a native temp calculator, so let's make one at 3am
+#Don't have a native temp calculator/converter, so let's make one at 3am
 tempf = input("Enter Temp (F) Here: ")
 resultC = float(tempf) - int(32)
 resultC2 = float(resultC) * int(5)
 resultC3 = float(resultC2) / int(9)
-print("Celcius: " )
+print("Celsius: " )
 print(resultC3 )
 #I'm adding Kelvin to try and find a niche with this
 #Code might be messy, but its 3am
